699a3c33b1988391735f68afcc1b8c6091642abd
Mark Samberg <mjsamber@ncsu.edu>
2020-11-29 01:11:13 +0000

    Added Shibboleth authentication

--- config/auth.php
+++ config/auth.php
@@ -70,6 +70,10 @@ return [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
         ],
+	'users' => [
+        	'driver' => 'shibboleth',
+        	'model'  => App\Models\User::class,
+    	],
 
         // 'users' => [
         //     'driver' => 'database',
