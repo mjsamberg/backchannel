72084238bb0506159f7fd03125eea2e95faea734
Mark Samberg <mjsamber@ncsu.edu>
2020-11-29 01:49:26 +0000

    Republished livewire.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
         @livewireStyles
 
         <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="{{ url(mix('js/app.js')) }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
